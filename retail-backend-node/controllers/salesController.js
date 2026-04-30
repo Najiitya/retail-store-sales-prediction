c86e58b9d11d4f45690fbdc0992f69125f9f2dac
Najith Nethwan <nethwannajith@gmail.com>
2026-04-30 11:11:59 +0530

    added fully functioning crud operations to sales

--- retail-backend-node/controllers/salesController.js
+++ retail-backend-node/controllers/salesController.js
@@ -23,7 +23,35 @@ const getAllSales =  async (req, res) =>{
     }
 };
 
+const updateSales = async (req, res) =>{
+    try{
+        const updateSale = await sale.findByIdAndUpdate(req.params.id, req.body, {new: true});
+
+        if(!updateSale){
+            return res.status(404).json({message: "Sale not found"});
+        }
+        res.status(200).json({message: "Sale updated succesfully!", data: updateSale});
+    }catch(error){
+        res.status(500).json({error: error.message});
+    }
+};
+
+const deleteSale = async (req, res) =>{
+    try{
+        const deleteSale = await sale.findByIdAndDelete(req.params.id);
+
+        if(!deleteSale){
+            return res.status(404).json({message: "Sale not found"});
+        }
+        res.status(200).json({message: "Sale deleted successfully"});
+    }catch(error){
+        res.status(500).json({error: error.message});
+    }
+};
+
 module.exports = {
     createSale,
-    getAllSales
+    getAllSales,
+    updateSales,
+    deleteSale
 };
--- retail-backend-node/routes/saleRoutes.js
+++ retail-backend-node/routes/saleRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 
-const {createSale, getAllSales} = require('../controllers/salesController');
+const {createSale, getAllSales, updateSales, deleteSale} = require('../controllers/salesController');
 
 router.post('/', createSale);
 router.get('/', getAllSales);
+router.put('/:id', updateSales);
+router.delete('/:id', deleteSale);
 
 module.exports = router;    
